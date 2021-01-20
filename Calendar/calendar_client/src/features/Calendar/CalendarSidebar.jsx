b200ded03f9b2f97c800001b4e2b83b14f63264f
grace dong <dongqiuyinggrace@gmail.com>
2021-01-20 15:40:57 +1100

    StylingSidebar

--- Calendar/calendar_client/src/features/Calendar/CalendarSidebar.jsx
+++ Calendar/calendar_client/src/features/Calendar/CalendarSidebar.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const CalendarSidebar = () => {
     return (
         <div className="calendar-sidebar">
-            Calendar Sidebar
+            <ul>
+                <li className="brand">Calendar</li>
+                <li><button className="toggle"><i className="fas fa-bars"></i></button></li>
+                <li>
+                    <button className="add-event">
+                        <i className="fas fa-plus"></i>
+                        <span>New Event</span>
+                    </button>
+                    
+                </li>
+            </ul>
         </div>
     )
 }
